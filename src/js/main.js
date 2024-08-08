6bbb98e685067e960d561c7ee62c9855920f93f8
nino <dankozero111@gmail.com>
2024-08-08 11:50:28 +0300

    [build]: has been created  basic structure

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// Import our custom CSS
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
