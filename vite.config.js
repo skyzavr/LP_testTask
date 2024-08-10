ccf4dbfb498a7d80064865e45cc548140f27fbd6
nino <dankozero111@gmail.com>
2024-08-10 14:48:59 +0300

    [build]: add alias, deps & etc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { resolve } from 'path';
-
-export default {
+import path from 'path';
+import { defineConfig } from 'vite';
+export default defineConfig({
   build: {
     outDir: '../dist',
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+      '@data': path.resolve(__dirname, 'src/js/data'),
+      '@helpers': path.resolve(__dirname, 'src/js/helpers'),
+    },
   },
   server: {
     port: 8080,
   },
-};
+});
