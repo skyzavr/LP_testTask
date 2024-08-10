dbb156828d93b11737e44e787a8a82996961c17e
nino <dankozero111@gmail.com>
2024-08-10 12:54:49 +0300

    [add]:toast component

---
+++ src/components/toast.ts
@@ -0,0 +1,26 @@
+type props = {
+  message: string;
+  type: string;
+  element: HTMLElement;
+};
+export const Toast = ({ message, type, element }: props) => {
+  const icons = {
+    error: '<i class="bi bi-exclamation"></i>',
+    success: '<i class="bi bi-check-lg"></i>',
+  };
+  element.innerHTML = `
+    <div class="toastArea ${type}">
+        <div class="message">
+            <div class="icon">
+                <div>${icons[type]}</div>
+            </div>
+            <div>${message}</div>
+        </div>
+        <span class="close" >
+            <i class="bi bi-x-lg"></i>
+        </span>
+    </div>`;
+  document.getElementsByClassName('close')[0].addEventListener('click', () => {
+    element.innerHTML = '';
+  });
+};
